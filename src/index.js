3e8945bd25edc2004eb7f34318ff828b53fa153d
TJManolis <40420536+TJManolis@users.noreply.github.com>
2018-07-03 11:29:54 -0600

    added redux stuff
    added most of the necessary components to use redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainPage from "./components/MainPage";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers/rootReducer";
+import { changeFirstName } from "./actions/actions";
 
 import "./styles.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<MainPage />, rootElement);
+let store = createStore(rootReducer);
+
+window.store = store;
+window.changeFirstName = changeFirstName;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MainPage />
+  </Provider>,
+  document.getElementById("root")
+);
