8eb409660832e2acb12a124ebdcb8272465a8569
TJManolis <40420536+TJManolis@users.noreply.github.com>
2018-07-10 17:53:18 -0600

    added component toggle
    can toggle between two different component sets on the main page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,22 +1,39 @@
 import React from "react";
 import Welcome from "../connectedComponents/ConnectedWelcome";
 import NameInput from "../connectedComponents/ConnectedNameInput";
+import Counter from "./Counter";
 
 class MainPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "Last Name"
+      firstName: "Last Name",
+      counterRender: false
     };
   }
 
+  clickToggleComponents = event => {
+    this.setState({ ...this.state, counterRender: !this.state.counterRender });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <Welcome lastName={this.state.firstName} />
-        <NameInput />
-      </div>
-    );
+    if (this.state.counterRender === false) {
+      return (
+        <div className="App">
+          <Welcome lastName={this.state.firstName} />
+          <NameInput />
+          <button onClick={this.clickToggleComponents}>Toggle</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>Welcome To the COUNTER APP! CLICK AWAY!!!!</h1>
+          <Counter />
+          <button onClick={this.clickToggleComponents}>Toggle</button>
+        </div>
+      );
+    }
   }
 }
 
